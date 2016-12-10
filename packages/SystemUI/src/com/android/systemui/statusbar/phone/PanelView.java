46fbe52261f4c26a817aa20420a4495966b6a52b
Anas Karbila <anaskarbila@aospa.co>
2016-12-10 18:40:23 +0100

    PanelView: add double tap to sleep on keyguard
    Change-Id: Ibd80dbaf749e891971f2f52a03a3d1ed2bf7222b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -26,6 +26,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.AsyncTask;
+import android.os.PowerManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.VibrationEffect;
@@ -33,6 +34,7 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.InputDevice;
 import android.view.MotionEvent;
 import android.view.View;
@@ -140,6 +142,8 @@ public abstract class PanelView extends FrameLayout {
     private boolean mIgnoreXTouchSlop;
     private boolean mExpandLatencyTracking;
 
+    private GestureDetector mDoubleTapGestureListener;
+
     protected void onExpandingFinished() {
         mBar.onExpandingFinished();
     }
@@ -198,6 +202,18 @@ public abstract class PanelView extends FrameLayout {
 
     public PanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mDoubleTapGestureListener = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent event) {
+                final PowerManager pm = (PowerManager) context.getSystemService(
+                        Context.POWER_SERVICE);
+                pm.goToSleep(event.getEventTime());
+                return true;
+            }
+        });
+
         mFlingAnimationUtils = new FlingAnimationUtils(context, 0.6f /* maxLengthSeconds */,
                 0.6f /* speedUpFactor */);
         mFlingAnimationUtilsClosing = new FlingAnimationUtils(context, 0.5f /* maxLengthSeconds */,
@@ -205,6 +221,7 @@ public abstract class PanelView extends FrameLayout {
         mFlingAnimationUtilsDismissing = new FlingAnimationUtils(context,
                 0.5f /* maxLengthSeconds */, 0.2f /* speedUpFactor */, 0.6f /* x2 */,
                 0.84f /* y2 */);
+
         mBounceInterpolator = new BounceInterpolator();
         mFalsingManager = FalsingManager.getInstance(context);
         mNotificationsDragEnabled =
@@ -396,6 +413,11 @@ public abstract class PanelView extends FrameLayout {
                 endMotionEvent(event, x, y, false /* forceCancel */);
                 break;
         }
+
+        if (mStatusBar.getBarState() == StatusBarState.KEYGUARD) {
+            mDoubleTapGestureListener.onTouchEvent(event);
+        }
+
         return !mGestureWaitForTouchSlop || mTracking;
     }
 
