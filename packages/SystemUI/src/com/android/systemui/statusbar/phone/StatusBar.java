0f427beb93cfd662e38d0570da68c85c760eec21
Park Ju Hyung <qkrwngud825@gmail.com>
2017-01-15 03:33:04 +0900

    SystemUI: Implement burn-in protection for status-bar/nav-bar items
    Devices with OLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    This new feature can be enabled by setting
    config_statusBarBurnInProtection to true.
    
    The shifting interval can be configured with config_shift_interval.
    The default shifting interval is 60 seconds.
    
    Forward-ported to oreo-mr1.
    
    Change-Id: I778576e26e9056b3127050541f335c168a81377d
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -227,6 +227,7 @@ import com.android.systemui.statusbar.phone.UnlockMethodCache.OnUnlockMethodChan
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BatteryController.BatteryStateChangeCallback;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
+import com.android.systemui.statusbar.policy.BurnInProtectionController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher;
@@ -419,6 +420,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected FingerprintUnlockController mFingerprintUnlockController;
     LightBarController mLightBarController;
     protected LockscreenWallpaper mLockscreenWallpaper;
+
+    private BurnInProtectionController mBurnInProtectionController;
 
     int mNaturalBarHeight = -1;
 
@@ -1094,6 +1097,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mStatusBarView.setBouncerShowing(mBouncerShowing);
                     setAreThereNotifications();
                     checkBarModes();
+                    mBurnInProtectionController =
+                        new BurnInProtectionController(mContext, this, mStatusBarView);
                 }).getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.status_bar_container, new CollapsedStatusBarFragment(),
@@ -5308,6 +5313,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             // Keyguard.
             mNotificationPanel.setTouchDisabled(true);
             mStatusBarWindow.cancelCurrentTouch();
+            if (mBurnInProtectionController != null) {
+                mBurnInProtectionController.stopSwiftTimer();
+            }
             if (mLaunchCameraOnFinishedGoingToSleep) {
                 mLaunchCameraOnFinishedGoingToSleep = false;
 
@@ -5341,6 +5349,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             mDozeServiceHost.stopDozing();
             updateVisibleToUser();
             updateIsKeyguard();
+            if (mBurnInProtectionController != null) {
+                mBurnInProtectionController.startSwiftTimer();
+            }
         }
     };
 
